3c07ceccf18932fad9ab71feeda95fc55b743852
kalash <kalashm24@gmail.com>
2020-05-16 14:41:36 +0545

    Added Login APi

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -16,6 +16,10 @@ from rest_framework import filters
 
 from profiles_api import permissions
 
+from rest_framework.authtoken.views import ObtainAuthToken
+
+from rest_framework.settings import api_settings
+
 
 
 class HelloApiView(APIView):
@@ -118,3 +122,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name','email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
